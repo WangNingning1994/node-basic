aa1912ee70b3b1431e1d7a2e47837b5475db09ad
Wang Ningning <wangningning@MacBook-Air.lan>
2020-07-29 21:42:40 +0800

    Add event handler.

--- server.js
+++ server.js
@@ -1,7 +1,17 @@
 const http = require('http');
 const requestListener = (req, res) => {
-  res.writeHead(200);
-  res.end('Hello, World!');
+  let body = [];
+  req.on('error', (err) => {
+    console.log(err);
+  }).on('data', (chunk) => {
+    // body.push(chunk.toString());
+    body.push(chunk);
+  }).on('end', () => {
+    body = Buffer.concat(body).toString();
+    console.log('body: ', body);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, World\n');
+  });
 }
 const server = http.createServer(requestListener);
 server.listen(8080);
